c780bbf6a2fa3b15dab2fa4f6be3b977328aa16c
Osman Dag <osman.dag@outlook.com>
2026-04-22 10:00:03 +0000

    version 1.5

--- tests/testthat/testthat-vht.R
+++ tests/testthat/testthat-vht.R
@@ -1,14 +1,15 @@
 library(testthat)
 library(vartest)
 
-test_that("adjusted.taha.test works", {
-  result <- adjusted.taha.test(Sepal.Length ~ Species, data = iris)
-  expect_s3_class(result, "vht")
-  expect_true(is.numeric(result$p.value))
-  expect_true(result$p.value >= 0 && result$p.value <= 1)
-  expect_true(is.numeric(result$statistic))
-  expect_s3_class(result$data, "data.frame")
-  expect_true(inherits(result$formula, "formula"))
+test_that("select_vartest works", {
+  result <- select_vartest(Sepal.Length ~ Species, data = iris)
+  expect_s3_class(result, "data.frame")
+  expect_true(is.numeric(result$power))
+  expect_true(all(result$power >= 0 & result$power <= 1))
+  expect_true(is.numeric(result$typeIerror))
+  expect_true(all(result$typeIerror >= 0 & result$typeIerror <= 1))
+  expect_true(is.numeric(result$adjpower))
+  expect_true(all(result$adjpower >= 0 & result$adjpower <= 1))
 })
 
 test_that("bartletts.test works", {
@@ -61,16 +62,6 @@ test_that("duran.test works", {
   expect_true(inherits(result$formula, "formula"))
 })
 
-test_that("adjusted.taha.test works", {
-  result <- adjusted.taha.test(Sepal.Length ~ Species, data = iris)
-  expect_s3_class(result, "vht")
-  expect_true(is.numeric(result$p.value))
-  expect_true(result$p.value >= 0 && result$p.value <= 1)
-  expect_true(is.numeric(result$statistic))
-  expect_s3_class(result$data, "data.frame")
-  expect_true(inherits(result$formula, "formula"))
-})
-
 test_that("fk.test works", {
   result <- fk.test(Sepal.Length ~ Species, data = iris)
   expect_s3_class(result, "vht")
@@ -103,16 +94,6 @@ test_that("mood.test works", {
 
 test_that("siegel.tukey.test works", {
   result <- siegel.tukey.test(Sepal.Length ~ Species, data = iris)
-  expect_s3_class(result, "vht")
-  expect_true(is.numeric(result$p.value))
-  expect_true(result$p.value >= 0 && result$p.value <= 1)
-  expect_true(is.numeric(result$statistic))
-  expect_s3_class(result$data, "data.frame")
-  expect_true(inherits(result$formula, "formula"))
-})
-
-test_that("taha.test works", {
-  result <- taha.test(Sepal.Length ~ Species, data = iris)
   expect_s3_class(result, "vht")
   expect_true(is.numeric(result$p.value))
   expect_true(result$p.value >= 0 && result$p.value <= 1)
